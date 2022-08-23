33d7c3686b479c69c6d9fc4d19f3ff13511ad78a
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-23 21:15:13 +0300

    Refactor actions\auth.js

--- src/global/actions/auth.js
+++ src/global/actions/auth.js
@@ -3,15 +3,11 @@ import { REGISTER_USER } from "../constants/actionTypes"
 
 export const registerNewUser = (userData) => async (dispatch) => {
   try {
-    const { data } = await api.registerNewUser(userData).then(() =>{
-      console.log(data);
-      dispatch({ type: REGISTER_USER, payload: data, errors: null})
-    }).catch((error) => {
+    await api.registerNewUser(userData).catch((error) => {
       console.log(error.response.data)
       dispatch({ type: REGISTER_USER, errors: error.response.data })
-    }
-    )
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
   }
 }
