7d0d021b5d2d0629c6efc17e0e9020e0b323efa5
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-24 22:19:37 +0300

    Implement login existing user api service

--- src/utils/api/APIService.js
+++ src/utils/api/APIService.js
@@ -5,6 +5,9 @@ const apiBaseUrl = process.env.REACT_APP_BASE_URL
 
 // api urls
 const registerUserUrl = `${apiBaseUrl}/auth/register`
+const loginExistingUserUrl = `${apiBaseUrl}/auth/login`
 
 // register user
 export const registerNewUser = (userData) => axios.post(registerUserUrl, userData)
+// login existing user
+export const loginExistingUser = (userData) => axios.post(loginExistingUserUrl, userData)
