0c8d61d19dd9f559df981ef4334f821a27d073d1
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-24 22:11:28 +0300

    Implement login user auth reducer

--- src/global/reducers/auth.js
+++ src/global/reducers/auth.js
@@ -1,10 +1,13 @@
-import { REGISTER_USER } from "../constants/actionTypes";
+import { LOGIN_USER, REGISTER_USER } from "../constants/actionTypes";
 
-export const authReducer = (state = { authData: null, errorResponse: null }, action) =>{
-  switch(action.type){
+export const authReducer = (state = { authData: null, errorResponse: null }, action) => {
+  switch (action.type) {
     case REGISTER_USER:
       return { ...state, authData: action.payload, errorResponse: action.errorResponse }
-    
+
+    case LOGIN_USER:
+      return { ...state, authData: action.payload, errorResponse: action.errorResponse }
+
     default:
       return state
   }
