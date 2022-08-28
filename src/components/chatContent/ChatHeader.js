8d860c14b90cb59dc9378203b0ec02d5e5a5b299
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-08-28 14:12:42 +0300

    Implement chat header

--- src/components/chatContent/ChatHeader.js
+++ src/components/chatContent/ChatHeader.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Avatar from "../avatar/Avatar"
 
 const ChatHeader = () => {
   return (
-    <div>ChatHeader</div>
+    <div className="content__header">
+      <div className="blocks">
+        <div className="current-chatting-user">
+          <Avatar isOnline="active"
+            image="https://c0.wallpaperflare.com/preview/500/556/924/doberman-dog-beauty-nature.jpg"
+          />
+          <p>Common Chat</p>
+        </div>
+      </div>
+
+      <div className="blocks">
+        <div className="settings">
+          <button className="btn-nobg">
+            <i className="fa fa-cog"></i>
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
