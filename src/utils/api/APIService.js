18dc57282bca08acf3125f0104f3b61d5edf995b
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-09-01 22:42:53 +0300

    Implement get existing chats api service

--- src/utils/api/APIService.js
+++ src/utils/api/APIService.js
@@ -6,8 +6,11 @@ const apiBaseUrl = process.env.REACT_APP_BASE_URL
 // api urls
 const registerUserUrl = `${apiBaseUrl}/auth/register`
 const loginExistingUserUrl = `${apiBaseUrl}/auth/login`
+const chatsUrl = `${apiBaseUrl}/chats`
 
 // register user
 export const registerNewUser = (userData) => axios.post(registerUserUrl, userData)
 // login existing user
 export const loginExistingUser = (userData) => axios.post(loginExistingUserUrl, userData)
+//get all existing chats
+export const getExistingChats = (userId) => axios.get(`${chatsUrl}/${userId}`)
