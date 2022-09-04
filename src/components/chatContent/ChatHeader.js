dddf0e9ce9d1002066acc0ec7a74990b42d32ae8
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-09-04 21:53:00 +0300

    Switch Chats

--- src/components/chatContent/ChatHeader.js
+++ src/components/chatContent/ChatHeader.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useSelector } from "react-redux"
 import Avatar from "../avatar/Avatar"
 
 const ChatHeader = () => {
+  const currentAuthenticatedUser = JSON.parse(localStorage.getItem('authenticatedUser'))
+  const chatsState = useSelector((state) => state.getChatsReducer.chats)
+  const currentChat = useSelector((state) => state.getChatsReducer.currentChat)
+  const [chatUser, setChatUser] = useState({})
+
+  useEffect(() => {
+    
+  })
+
+  useEffect(() => {
+    const friendId = currentChat?.members.find((id) => id !== currentAuthenticatedUser._id)
+
+    const getUser = async () => {
+      try {
+        const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/users?userId=${friendId}`)
+        setChatUser(data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+
+    getUser()
+  }, [currentAuthenticatedUser._id, currentChat?.members])
+
   return (
     <div className="content__header">
       <div className="blocks">
         <div className="current-chatting-user">
           <Avatar isOnline="active"
-            image="https://c0.wallpaperflare.com/preview/500/556/924/doberman-dog-beauty-nature.jpg"
+            image={chatUser.profilePicture}
           />
-          <p>Common Chat</p>
+          <p>{chatUser.username}</p>
         </div>
       </div>
 
