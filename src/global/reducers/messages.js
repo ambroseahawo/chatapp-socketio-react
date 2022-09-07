c75a4d14478699dece60600380b4080150483752
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-09-07 20:32:28 +0300

    Implement post message reducer

--- src/global/reducers/messages.js
+++ src/global/reducers/messages.js
@@ -1,9 +1,19 @@
-import { GET_MESSAGES } from "../constants/actionTypes";
+import { GET_MESSAGES, POST_MESSAGE } from "../constants/actionTypes";
 
 export const getMessagesReducer = (state = { messages: [] }, action) => {
   switch (action.type) {
     case GET_MESSAGES:
       return { ...state, messages: action.payload }
+
+    default:
+      return state
+  }
+}
+
+export const postMessageReducer = (state = { message: null }, action) => {
+  switch (action.type) {
+    case POST_MESSAGE:
+      return { ...state, message: action.payload }
 
     default:
       return state
